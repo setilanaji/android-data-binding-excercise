8127440ca12f0b06e11a85bc20c908a8a24ca224
PC-2 <setyajiyudha299@gmail.com>
2020-11-12 12:36:36 +0700

    update

--- app/src/main/java/com/example/androiddatabinding/ui/activity/SecondActivity.kt
+++ app/src/main/java/com/example/androiddatabinding/ui/activity/SecondActivity.kt
@@ -26,15 +26,13 @@ class SecondActivity : AppCompatActivity() {
 
         binding.editTextTextPersonName.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("Not yet implemented")
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                TODO("Not yet implemented")
             }
 
             override fun afterTextChanged(s: Editable?) {
-                userViewModel.setAllUser()
+
             }
 
         })
